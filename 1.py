42b5bee8bb9b76cf09eae65ca2334514bafa5048
shamyW <513961560@qq.com>
2019-02-18 11:44:32 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+
+#import standard
+#import standard
+#import standard
+
+#import others
+#import others
+#import others
+
+#import myself
+#import myself
+#import myself
+
+
+class Test(object):
+
+    def __init__(self):
+        pass
+
+    def output(self):
+        print('i am a tester')
+        print('i am a tester')
+        print('i am a tester')
+        return 1
+
+
+def function(num=1):
+    '''
+    this is a function for testing
+    :return: num
+    '''
+
+    return num
+
+
+def addxy(x=1, y=2):
+    '''
+    this function used for addxy
+    :param x: the first input
+    :param y: the second input
+    :return: the result of addxying x and y
+    '''
+
+    return x + y
+
+
+
+print(function(1))
+
+print(function.__doc__)
+
+dic = {'key': 'value'}
+
+print(dic['key'])
+
+print(addxy())
+
+print(addxy.__doc__)
+
+
+a = Test()
+print(a.output())
---
+++ autopep8testing.py
@@ -0,0 +1,10 @@
+import os
+
+
+def funtion(x):
+
+    x = x + 2
+    return x
+
+
+print(function(1))
